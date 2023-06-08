a4d7fdb699d3b75cf5af1fa84cf99e8a00a07ee6
ahmadadelnassar <ahmad.adel.nassar@gmail.com>
2023-06-08 11:02:35 +0000

    add docker file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
 export default function Index() {
-  return <div>Hello World</div>;
-}
+  return <div><div>Node js is a run time environment. Comes with its pacjage manger npm</div><div>React is a package that helps design web site front ends</div>
+    <div>vite is a builder that quickly builds web sites</div></div>;
+    }
--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(() => {
       ],
     },
     server: {
-      https: true,
+      https: false,
     },
     plugins: [
       react(),
