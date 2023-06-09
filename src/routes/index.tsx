3dd12a59a50d99db499d2091a890d49304323099
ahmadadelnassar <ahmad.adel.nassar@gmail.com>
2023-06-09 11:21:53 +0000

    fixes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
 export default function Index() {
-  return <div><div>Hi, I am a web app orchestrates by K8s, hoset on AKS, using docker image from ACR.</div><div>Ahmad Created me!</div>
+  return <div><div>Hi, I am a web app orchestrates by K8s, hosted through AKS, using docker image from ACR.</div><div>Ahmad Created me!</div>
     <div>Man, He is really a good devop cloud engineer!</div></div>;
     }
