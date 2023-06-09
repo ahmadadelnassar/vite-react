4de847fade023ad7fd13cb9368d569fa0835c60f
ahmadadelnassar <ahmad.adel.nassar@gmail.com>
2023-06-09 10:19:33 +0000

    modify setup

--- setup.bash
+++ setup.bash
@@ -1,2 +1,4 @@
 # install azure cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
